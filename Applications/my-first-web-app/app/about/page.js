5cd51a914706d9542bf95cbe95cc8ea41c4efacc
PavanRedla <pavankalyanredla42@gmail.com>
2024-07-05 21:45:11 +0530

    my first web app

---
+++ Applications/my-first-web-app/app/about/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>About Component</div>;
+};
+
+export default page;
---
+++ Applications/my-first-web-app/app/home/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Home Component</div>;
+};
+
+export default page;
