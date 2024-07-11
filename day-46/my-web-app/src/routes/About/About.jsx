1f86800210846512b0ebe2be72e76a0f66f833ca
PavanRedla <pavankalyanredla42@gmail.com>
2024-07-11 15:08:59 +0530

    library installation in our app

--- day-46/my-web-app/src/routes/About/About.jsx
+++ day-46/my-web-app/src/routes/About/About.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { name, location, Bus, add } from "my-own-react-library";
 
 export const About = () => {
-  return <div>About</div>;
+  const busObj = new Bus();
+  return (
+    <div>
+      <h1>About</h1>
+
+      <h3>
+        {name} --- {location}
+      </h3>
+      <h3>{add(10, 20)}</h3>
+      <h3>
+        {busObj.fw} - {busObj.bw} - {busObj.totalWheels()} - {busObj.getColor()}{" "}
+      </h3>
+    </div>
+  );
 };
