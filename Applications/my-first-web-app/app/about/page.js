ed18ec7d53e2f26926bd7652a6baec0a96e91be4
Redla Pavan Kalyan <153701786+PavanRedla@users.noreply.github.com>
2024-07-23 07:59:02 +0530

    commit

--- Applications/my-first-web-app/app/about/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>About Component</div>;
-};
-
-export default page;
--- Applications/my-first-web-app/app/home/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Home Component</div>;
-};
-
-export default page;
